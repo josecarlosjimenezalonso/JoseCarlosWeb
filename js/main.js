5aab93fbe84091288f3e2639d4542bce3250dbcf
josecarlosjimenezalonso <josecarlos.jimenez.alonso@alumnos.upm.es>
2018-02-26 16:19:18 +0100

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(funcion(){
+
+	$(window).scroll(function(){
+
+
+		var barra = $(window).scrollTop();
+		var posicion = barra * 0.10;
+
+
+		$('body').css({
+			'background-position': '0 -' +posicion+ 'px'
+
+		});
+
+	});
+
+
+
+});
